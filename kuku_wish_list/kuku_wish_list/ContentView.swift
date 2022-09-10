1cd7e866e34d190813d662a146a949836792c350
nexlice <auspiciouswho47@gmail.com>
2022-09-10 17:47:10 +0900

    hae-chi-wot-na?

---
+++ kuku_wish_list/kuku_wish_list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kuku_wish_list
+//
+//  Created by Andrew Wonwhoo NAH on 2022/09/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kuku_wish_list/kuku_wish_list/kuku_wish_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  kuku_wish_listApp.swift
+//  kuku_wish_list
+//
+//  Created by Andrew Wonwhoo NAH on 2022/09/10.
+//
+
+import SwiftUI
+
+@main
+struct kuku_wish_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
