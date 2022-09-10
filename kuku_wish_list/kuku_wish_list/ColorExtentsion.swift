a7832f9007429c72f8ef01f27b3a2e7293430add
HJHA <olivia82597@gmail.com>
2022-09-11 03:15:35 +0900

    colorextension : 색상코드, Hashtag 구현 중

---
+++ kuku_wish_list/kuku_wish_list/ColorExtentsion.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtentsion.swift
+//  kuku_wish_list
+//
+//  Created by 하현지 on 2022/09/11.
+//
+
+import Foundation
--- kuku_wish_list/kuku_wish_list/Hashtag.swift
+++ kuku_wish_list/kuku_wish_list/Hashtag.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+
+struct Hashtag_View: View {
+    var body: some View {
+        
+        // Start of hashtag
+        ZStack(){
+            Text("")
+                .frame(width: 84, height: 36)
+                .background(Color.gray)
+                .cornerRadius(18)
+//                .padding(5)
+            
+            // Start of color circle, text
+            HStack(){
+                Circle()
+                    .frame(width: 24, height: 24)
+                
+                Text("work")
+
+            }
+            // End of color circle, text
+        }
+        // End of hashtag
+    }
+}
+
+struct Hashtag_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            Hashtag_View()
+        }
+    }
+}
