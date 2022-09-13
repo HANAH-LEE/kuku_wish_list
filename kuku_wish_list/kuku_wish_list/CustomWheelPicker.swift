d8ae17c0453e0e15932e4f7f95f9d0c238b92d24
nexlice <auspiciouswho47@gmail.com>
2022-09-13 17:56:22 +0900

    주석...

--- kuku_wish_list/kuku_wish_list/CustomWheelPicker.swift
+++ kuku_wish_list/kuku_wish_list/CustomWheelPicker.swift
@@ -18,9 +18,6 @@ struct CustomPickerView: View{
 
 
 struct CustomPicker: UIViewRepresentable{
-    func makeCoordinator() -> Coordinator {
-        return CustomPicker.Coordinator(parent1: self )
-    }
     
     func makeUIView(context: UIViewRepresentableContext<CustomPicker>) -> UIPickerView{
         let picker = UIPickerView()
@@ -33,6 +30,21 @@ struct CustomPicker: UIViewRepresentable{
         
     }
     
+    
+    //coordinator의 의미
+    //https://ally10.tistory.com/44
+    //coordinatorsms UIKit -> SwiftUI로의 데이터 전달이다
+    //coordinator의 구현 방법
+    //https://sweetdev.tistory.com/447
+    
+    func makeCoordinator() -> Coordinator {
+        return CustomPicker.Coordinator(parent1: self )
+    }
+    
+    //각 struct 객체마다 coordinator가 필요한 경우가 있으므로
+    //struct안에 class를 정하는게 좋다.
+    //class name은 꼭 Coordinator가 아니어도 된다.
+    //makeCoordinator를 이용할 것이기 때문.
     class Coordinator : NSObject, UIPickerViewDelegate, UIPickerViewDataSource{
         var parent : CustomPicker
         init(parent1 : CustomPicker){
