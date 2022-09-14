a49580efba084282c3e6aa28e57faa9caa79993e
nexlice <auspiciouswho47@gmail.com>
2022-09-14 18:05:50 +0900

    custom wheel picker WIP

--- kuku_wish_list/kuku_wish_list/CustomWheelPicker.swift
+++ kuku_wish_list/kuku_wish_list/CustomWheelPicker.swift
@@ -11,13 +11,30 @@ import SwiftUI
 //https://www.youtube.com/watch?v=uPFk2BuNT5M
 
 struct CustomPickerView: View{
+    @State var selected = "Monday"
     var body: some View{
-        CustomPicker()
+        ZStack{
+            Color.lightgray.frame(width: UIScreen.main.bounds.width, height: 300)
+            VStack(){
+                HStack(){
+                    Spacer().frame(width: 50)
+                    Text("select a date")
+                        .font(.custom("Montserrat-Medium", size: 25))
+                    Spacer()
+                    Text("save")
+                        .font(.custom("Montserrat-Medium", size: 25))
+                    Spacer().frame(width: 50)
+                }
+                CustomPicker(selected: $selected)
+            }
+        }
     }
 }
 
 
 struct CustomPicker: UIViewRepresentable{
+    
+    @Binding var selected : String
     
     func makeUIView(context: UIViewRepresentableContext<CustomPicker>) -> UIPickerView{
         let picker = UIPickerView()
@@ -47,6 +64,7 @@ struct CustomPicker: UIViewRepresentable{
     //makeCoordinator를 이용할 것이기 때문.
     class Coordinator : NSObject, UIPickerViewDelegate, UIPickerViewDataSource{
         var parent : CustomPicker
+        
         init(parent1 : CustomPicker){
             parent = parent1
         }
@@ -56,18 +74,50 @@ struct CustomPicker: UIViewRepresentable{
         }
         
         func numberOfComponents(in pickerView: UIPickerView) -> Int {
-            return 1
+            return 3
         }
         
-        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        //        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        //
+        //            return data[row]
+        //        }
+        
+        func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView{
+            let view = UIView(frame: CGRect(x:0 , y:0 , width: UIScreen.main.bounds.width/4, height: 60))
             
-            return data[row]
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+            
+            label.text = data[row]
+            label.textColor = .white
+            label.textAlignment = .center
+            label.font = .systemFont(ofSize: 25, weight: .bold)
+            
+            view.backgroundColor = .red
+            
+            view.clipsToBounds = true
+            view.layer.cornerRadius = view.bounds.height/2
+            
+            view.addSubview(label)
+            return view
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat{
+            return UIScreen.main.bounds.width/4
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+            return 60
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            self.parent.selected = data[row]
         }
         
     }
 }
 
 var data = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 struct CustomPickerPreviews: PreviewProvider {
     static var previews: some View {
